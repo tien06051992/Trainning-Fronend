ad401df094ce490f4f8876d8b20c53b341e25e9f
nguyenguyen <nguyenit95@gmail.com>
2016-06-22 15:30:06 +0700

    22/06 updated

---
+++ Trang/js/main.js
@@ -0,0 +1,14 @@
+// hoverPic(i){
+// 	var imgId = 'img' + i;
+// 	var divId = 'div' + i;
+// 	document.getElementById(i).style.visibility = 'visible';
+// 	document.getElementById(i).style.visibility = 'visible';
+// }
+
+// notHoverPic(i){
+// 	var imgId = 'img' + i;
+// 	var divId = 'div' + i;
+// 	document.getElementById(i).style.visibility = 'hidden';
+// 	document.getElementById(i).style.visibility = 'hidden';
+// }
+
