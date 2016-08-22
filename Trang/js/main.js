298c6c9f3b5a42b43468727b2d65b3f14394a479
nguyenguyen <nguyenit95@gmail.com>
2016-08-22 13:49:37 +0700

    updated 22/08

--- Trang/js/main.js
+++ Trang/js/main.js
@@ -1,25 +1,16 @@
-// hoverPic(i){
-// 	var imgId = 'img' + i;
-// 	var divId = 'div' + i;
-// 	document.getElementById(i).style.visibility = 'visible';
-// 	document.getElementById(i).style.visibility = 'visible';
-// }
-
-// notHoverPic(i){
-// 	var imgId = 'img' + i;
-// 	var divId = 'div' + i;
-// 	document.getElementById(i).style.visibility = 'hidden';
-// 	document.getElementById(i).style.visibility = 'hidden';
-// }
 
 // external js: isotope.pkgd.js, packery-mode.pkgd.js
 $(document).ready(function() {
 
 
 $('.grid').isotope({
-  layoutMode: 'packery',
   itemSelector: '.grid-item',
-});
-
+  percentPosition: false,
+  masonry: {
+    columnWidth: 224,
+    isFitWidth: true,
+    gutter: 0
+   }
+  })
 });
 
